d22fd308af74c3362cf9d86174373c377a405b01
aakashjain <aakashj19@gmail.com>
2014-08-10 12:30:34 +0530

    webapp

---
+++ web.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+require 'net/http'
+require 'digest'
+require 'nokogiri'
+
+SCROBBLE_ROOT = 'http://ws.audioscrobbler.com/2.0/'
+API_KEY = '***REMOVED***'
+SECRET = '***REMOVED***'
+SESSION_KEY = '***REMOVED***'
+
+get '/scrobble' do
+	data = {
+		'method' => 'track.scrobble',
+		'api_key' => API_KEY,
+		'sk' => SESSION_KEY,
+		'timestamp' => params[:timestamp],
+		'artist' => params[:artist],
+		'track' => params[:track],
+		'album' => params[:album]
+	}
+	sig = data.sort.map{|key, val| key + val}.join + SECRET
+	data['api_sig'] = Digest::MD5.hexdigest(sig)
+	response = Net::HTTP.post_form(URI.parse(SCROBBLE_ROOT), data)
+	Nokogiri::Slop(response.body).lfm.attr('status')
+end
