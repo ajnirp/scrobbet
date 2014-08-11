f329bcfbf6ab96cb45d8c1254ef72a11237c78d0
aakashjain <aakashj19@gmail.com>
2014-08-11 23:34:25 +0530

    token/session methods in web.rb

--- web.rb
+++ web.rb
@@ -7,6 +7,16 @@ SCROBBLE_ROOT = 'http://ws.audioscrobbler.com/2.0/'
 API_KEY = ##
 SECRET = ##
 
+helpers do
+
+	def sendrequest(data)
+		sig = data.sort.map{|key, val| key + val}.join + SECRET
+		data['api_sig'] = Digest::MD5.hexdigest(sig)
+		return Net::HTTP.post_form(URI.parse(SCROBBLE_ROOT), data)
+	end
+
+end
+
 post '/scrobble' do
 	data = {
 		'method' => 'track.scrobble',
@@ -17,8 +27,26 @@ post '/scrobble' do
 		'track' => params[:track],
 		'album' => params[:album]
 	}
-	sig = data.sort.map{|key, val| key + val}.join + SECRET
-	data['api_sig'] = Digest::MD5.hexdigest(sig)
-	response = Net::HTTP.post_form(URI.parse(SCROBBLE_ROOT), data)
+	response = sendrequest(data)
 	Nokogiri::Slop(response.body).lfm.attr('status')
 end
+
+post '/gettoken' do
+	data = 	{
+		'method' => 'auth.getToken', 
+		'api_key' => API_KEY
+	}
+	response = sendrequest(data)
+	Nokogiri::Slop(response.body).lfm.token.content
+end
+
+post '/getsession' do
+	data = {
+		'method' => 'auth.getSession',
+		'api_key' => API_KEY,
+		'token' => params[:token]
+	}
+	response = sendrequest(data)
+	Nokogiri::Slop(response.body).lfm.session.key.content
+end
+
