3e3ddee9ab7f598ff543eba83747993b8d1ed2ba
Rohan Prinja <rohan.prinja@gmail.com>
2014-08-13 19:45:05 +0530

    replace tabs with spaces because MUH RUBY

--- web.rb
+++ web.rb
@@ -8,40 +8,40 @@ SECRET = ##
 
 helpers do
 
-	def sendrequest(data)
-		sig = data.sort.map{|key, val| key + val}.join + SECRET
-		data['api_sig'] = Digest::MD5.hexdigest(sig)
-		return Net::HTTP.post_form(URI.parse(SCROBBLE_ROOT), data)
-	end
+  def sendrequest(data)
+    sig = data.sort.map{|key, val| key + val}.join + SECRET
+    data['api_sig'] = Digest::MD5.hexdigest(sig)
+    return Net::HTTP.post_form(URI.parse(SCROBBLE_ROOT), data)
+  end
 
 end
 
 post '/scrobble' do
-	data = {
-		'method' => 'track.scrobble',
-		'api_key' => API_KEY,
-		'sk' => params[:sk],
-		'timestamp' => params[:timestamp],
-		'artist' => params[:artist],
-		'track' => params[:track],
-		'album' => params[:album]
-	}
-	return sendrequest(data).body
+  data = {
+    'method' => 'track.scrobble',
+    'api_key' => API_KEY,
+    'sk' => params[:sk],
+    'timestamp' => params[:timestamp],
+    'artist' => params[:artist],
+    'track' => params[:track],
+    'album' => params[:album]
+  }
+  return sendrequest(data).body
 end
 
 post '/gettoken' do
-	data = 	{
-		'method' => 'auth.getToken', 
-		'api_key' => API_KEY
-	}
-	return sendrequest(data).body
+  data =  {
+    'method' => 'auth.getToken', 
+    'api_key' => API_KEY
+  }
+  return sendrequest(data).body
 end
 
 post '/getsession' do
-	data = {
-		'method' => 'auth.getSession',
-		'api_key' => API_KEY,
-		'token' => params[:token]
-	}
-	return sendrequest(data).body
+  data = {
+    'method' => 'auth.getSession',
+    'api_key' => API_KEY,
+    'token' => params[:token]
+  }
+  return sendrequest(data).body
 end
